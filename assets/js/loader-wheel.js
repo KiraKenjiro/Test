7c9d8c928d3cead9f22824fd24e8116f63d24b85
Kira Kenjiro <kirakenjiro+github@gmail.com>
2024-05-03 11:24:30 +0100

    Delete assets directory

--- assets/js/loader-wheel.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function () {
-	const loader = document.querySelector(".loader");
-	loader.style.visibility = "hidden";
-
-	const body = document.querySelector("body");
-	body.style.visibility = "visible";
-});
